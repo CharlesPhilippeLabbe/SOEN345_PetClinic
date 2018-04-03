f6ee9822f0a653476efdc96a5431da1bb6d05890
CharlesPhilippeLabbe <c.p.labbe@me.com>
2018-04-02 21:06:53 -0400

    Managed to make the forklift (issue with queries) work, add @Transactional. The forklift worked. fixing issues with the 'select' queries

--- src/main/java/org/springframework/samples/petclinic/owner/newdata/CustomOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/owner/newdata/CustomOwnerRepositoryImpl.java
@@ -6,6 +6,7 @@ import org.springframework.samples.petclinic.owner.Pet;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
 import java.util.Collection;
 
 public class CustomOwnerRepositoryImpl implements CustomOwnerRepository{
@@ -13,11 +14,13 @@ public class CustomOwnerRepositoryImpl implements CustomOwnerRepository{
     @PersistenceContext
     EntityManager entityManager;
 
+
     /**
      * @Query("SELECT DISTINCT owner FROM new_owner left join fetch owner.pets WHERE owner.lastName LIKE :lastName%")
      * @param lastName
      * @return
      */
+    @Transactional
     @Override
     public Collection<Owner> findByLastName(String lastName) {
         Query query = entityManager.createNativeQuery("SELECT DISTINCT id FROM new_owners WHERE last_name LIKE ?");
@@ -30,10 +33,11 @@ public class CustomOwnerRepositoryImpl implements CustomOwnerRepository{
      * @param id
      * @return
      */
+    @Transactional
     @Override
     public Owner findById(Integer id) {
-        Query query = entityManager.createNativeQuery("SELECT 1 FROM new_owners  WHERE id = ?");
-        query.setParameter(1, id);
+        Query query = entityManager.createNativeQuery("SELECT 1 FROM new_owners  WHERE id = :id");
+        query.setParameter("id", id);
         Owner owner = (Owner)query.getSingleResult();
 
         query = entityManager.createNativeQuery("SELECT * FROM new_pets WHERE owner = ?");
@@ -49,17 +53,22 @@ public class CustomOwnerRepositoryImpl implements CustomOwnerRepository{
      * INSERT IGNORE INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023')
      * @param owner
      */
+    @Transactional
     @Override
     public void save(Owner owner) {
-        System.out.println("Executing custom query");
-       /* Query query = entityManager.createNativeQuery("INSERT IGNORE INTO new_owners VALUES ?");
-        query.setParameter(1, owner.getId());
-        query.setParameter(2, owner.getFirstName());
-        query.setParameter(3, owner.getLastName());
-        query.setParameter(4, owner.getAddress());
-        query.setParameter(5, owner.getCity());
-        query.setParameter(6, owner.getTelephone());
 
-        query.getSingleResult();*/
+
+        Query query = entityManager.createNativeQuery("INSERT INTO new_owners(id, first_name, last_name, address, city, telephone)" +
+            " VALUES(:id,:first_name,:last_name,:address,:city,:telephone)");
+        query.setParameter("id", owner.getId());
+        query.setParameter("first_name", owner.getFirstName());
+        query.setParameter("last_name", owner.getLastName());
+        query.setParameter("address", owner.getAddress());
+        query.setParameter("city", owner.getCity());
+        query.setParameter("telephone", owner.getTelephone());
+        System.out.println("Executing custom query");
+        query.executeUpdate();
+        System.out.println("Executed custom query");
+
     }
 }
